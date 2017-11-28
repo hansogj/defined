5d41e2a5d32608f7943858e342847820e86a3113
Hans Ole Gjerdrum <hansogj@gmail.com>
2017-11-28 23:20:47 +0100

    bundle-config

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # defined
 Cheks if properties or variables are defined
+
+
+```bash
+npm i array.defined
+```
+
+
+## Publish
+
+```bash
+npm version [<newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease]
+git push --follow-tags
+npm publish
+```
--- tsify.js
+++ tsify.js
@@ -1,8 +1,10 @@
 var browserify = require("browserify");
 var tsify = require("tsify");
 
+
 browserify()
   .add("./src/polyfill.ts")
-  .plugin("tsify", {project:"tsconfig.json"})
+  .plugin("tsify", {project:"tsconfig.bundle.json"})
+//  .plugin("tsify", config)
   .bundle()
   .pipe(process.stdout);
